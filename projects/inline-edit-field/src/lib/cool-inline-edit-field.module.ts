16f447e70e8e65bc8100602ed2559d9f16776683
Hacklone <toakak@gmail.com>
2018-12-08 14:55:23 +0100

    Fix inline edit

--- projects/inline-edit-field/src/lib/cool-inline-edit-field.module.ts
+++ projects/inline-edit-field/src/lib/cool-inline-edit-field.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome';
 
 import { CoolInlineEditFieldComponent } from './cool-inline-edit-field.component';
 import { CoolInlineEditFieldInputDirective } from './cool-inline-edit-field-input.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
+    CommonModule,
     FormsModule,
 
     Angular2FontawesomeModule,
